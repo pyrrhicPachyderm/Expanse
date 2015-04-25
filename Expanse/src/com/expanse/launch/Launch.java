b136f9c7eb03bdd2d609e9a2048aed448c51dfb1
xenoframium <xenoframium@gmail.com>
2015-04-26 11:09:29 +1200

    Launch will now call loadMods() function

--- Expanse/src/com/expanse/launch/Launch.java
+++ Expanse/src/com/expanse/launch/Launch.java
@@ -2,13 +2,18 @@ package com.expanse.launch;
 
 import java.io.File;
 
+import com.expanse.exception.ModUnsatisfiedDependencyException;
 import com.expanse.modloader.ModLoader;
 
 public class Launch {
 	
 	public static void main(String[] args){
 		System.setProperty("org.lwjgl.librarypath", new File("lib/natives").getAbsolutePath());
-		//ModLoader.initMods();
+		try {
+			ModLoader.loadMods(args[0]);
+		} catch (ModUnsatisfiedDependencyException e) {
+			e.printStackTrace();
+		}
 	}
 	
 }
