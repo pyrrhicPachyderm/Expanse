a1d9a9911183da6a112f492bb2162766f506cfde
xenoframium <xenoframium@gmail.com>
2015-04-26 11:00:18 +1200

    Fix Main function so it will actually launch.

--- Expanse/src/com/expanse/launch/Launch.java
+++ Expanse/src/com/expanse/launch/Launch.java
@@ -2,10 +2,13 @@ package com.expanse.launch;
 
 import java.io.File;
 
+import com.expanse.modloader.ModLoader;
+
 public class Launch {
 	
-	public static void main(){
+	public static void main(String[] args){
 		System.setProperty("org.lwjgl.librarypath", new File("lib/natives").getAbsolutePath());
+		//ModLoader.initMods();
 	}
 	
 }
