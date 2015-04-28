4700481c499b3999c9b0cd56f9a0d0c89298fa80
xenoframium <xenoframium@gmail.com>
2015-04-28 16:32:41 +1200

    Fix Launch Crash

--- Expanse/src/com/expanse/launch/Launch.java
+++ Expanse/src/com/expanse/launch/Launch.java
@@ -9,11 +9,11 @@ public class Launch {
 	
 	public static void main(String[] args){
 		System.setProperty("org.lwjgl.librarypath", new File("lib/natives").getAbsolutePath());
-		try {
+		/*try {
 			ModLoader.loadMods(args[0]);
 		} catch (ModUnsatisfiedDependencyException e) {
 			e.printStackTrace();
-		}
+		}*/
 	}
 	
 }
