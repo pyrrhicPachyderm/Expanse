b7f1083aba671daadaed979a2d19ee96fb06bad3
xenoframium <xenoframium@gmail.com>
2015-05-29 23:32:46 +1200

    Add Maths to Calculate if Point is in Triangle

--- Expanse/src/com/expanse/utils/VectorMath.java
+++ Expanse/src/com/expanse/utils/VectorMath.java
@@ -30,4 +30,26 @@ public class VectorMath {
 		return (vec1.x * vec2.x + vec1.y * vec2.y);
 	}
 	
+	public static float distVectorEnds(Vector2f vec1, Vector2f vec2){
+		return (float) (Math.sqrt((vec1.x - vec2.x) * (vec1.x - vec2.x) + (vec1.y - vec2.y) * (vec1.y - vec2.y)));
+	}
+	
+	public static float norm(int type, Vector2f vec){
+		if(type == 1){
+			return vec.x + vec.y;
+		}
+		return (float) Math.pow(Math.pow(vec.x, type) + Math.pow(vec.y, type), 1 / type);
+	}
+	
+	public static float infNorm(Vector2f vec){
+		return Math.max(vec.x, vec.y);
+	}
+	
+	/*
+	 * Create vector from Coordinate co1 to co2
+	 */
+	public static Vector2f vectorFromCoords(Vector2f co1, Vector2f co2){
+		return new Vector2f(co2.x - co1.x, co2.y - co1.y);
+	}
+	
 }
