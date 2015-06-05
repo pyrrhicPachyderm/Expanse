5bafea607775f78a5306a1cc78ba517b06028a25
cycle365 <cycle365@gmail.com>
2015-06-06 10:11:39 +1200

    Add a bit of fake world gen

--- Expanse/src/com/expanse/worldgeneration/WorldGeneration.java
+++ Expanse/src/com/expanse/worldgeneration/WorldGeneration.java
@@ -9,14 +9,15 @@ import com.expanse.resources.Resources;
 public class WorldGeneration {
 	
 	public static void generateWorld(){ //a placeholder right now
+		//name, resources(, coreResources), orbitR, r, m(, breathP, atmoD, surfaceTemp)(, isWormhole)
 		CelestialNode centralBody = new StellarMassBody("Sol", new Resources(), 0, 1, 1, false);
-		centralBody.addOrbitingObject(new TerrestrialPlanetaryMassBody("Mercury", new Resources(), new Resources(100000, 0, 0, 0), 1, 1, 1, 0, 0, -200));
-		centralBody.addOrbitingObject(new TerrestrialPlanetaryMassBody("Venus", new Resources(), new Resources(100000, 0, 0, 0), 1, 1, 1, 0, 0, -200));
+		centralBody.addOrbitingObject(new TerrestrialPlanetaryMassBody("Mercury", new Resources(), new Resources(100000, 0, 0, 0), 0.4, 0.383, 0.055, 0, 0, 180));
+		centralBody.addOrbitingObject(new TerrestrialPlanetaryMassBody("Venus", new Resources(), new Resources(100000, 0, 0, 0), 0.7, 0.95, 0.815, 0.027, 92, 200));
 		centralBody.addOrbitingObject(new TerrestrialPlanetaryMassBody("Earth", new Resources(), new Resources(100000, 0, 0, 0), 1, 1, 1, 1, 1, 298));
-		centralBody.addOrbitingObject(new TerrestrialPlanetaryMassBody("Mars", new Resources(), new Resources(100000, 0, 0, 0), 1, 1, 1, 0, 0, -200));
-		centralBody.addOrbitingObject(new GasPlanetaryMassBody("Jupiter", new Resources(), 1, 1, 1));
-		centralBody.addOrbitingObject(new GasPlanetaryMassBody("Saturn", new Resources(), 1, 1, 1));
-		centralBody.addOrbitingObject(new GasPlanetaryMassBody("Uranus", new Resources(), 1, 1, 1));
-		centralBody.addOrbitingObject(new GasPlanetaryMassBody("Neptune", new Resources(), 1, 1, 1));
+		centralBody.addOrbitingObject(new TerrestrialPlanetaryMassBody("Mars", new Resources(), new Resources(100000, 0, 0, 0), 1.5, 1, 1, 0, 0, 200));
+		centralBody.addOrbitingObject(new GasPlanetaryMassBody("Jupiter", new Resources(), 50, 300, 300));
+		centralBody.addOrbitingObject(new GasPlanetaryMassBody("Saturn", new Resources(), 100, 200, 100));
+		centralBody.addOrbitingObject(new GasPlanetaryMassBody("Uranus", new Resources(), 150, 100, 75));
+		centralBody.addOrbitingObject(new GasPlanetaryMassBody("Neptune", new Resources(), 200, 150, 50));
 	}
 }
