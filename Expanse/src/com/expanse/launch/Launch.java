177b7bb033c530de54d2bc4a244b23d78bd79a91
xenoframium <xenoframium@gmail.com>
2015-06-22 13:53:40 +1200

    Repair Launch

--- Expanse/src/com/expanse/launch/Launch.java
+++ Expanse/src/com/expanse/launch/Launch.java
@@ -10,7 +10,6 @@ public class Launch {
 	
 	public static void main(String[] args){
 		System.setProperty("org.lwjgl.librarypath", new File("lib/lwjgl/native").getAbsolutePath());
-		
 		Thread modloaderThread = new Thread(new Runnable(){
 
 			@Override
